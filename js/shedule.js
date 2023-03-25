b9cc382ca526d323ac0c66dc4e834025f3964973
Maxim <maximvasiliyev314@gmail.com>
2023-03-26 00:49:42 +0300

    gpt_version_commit

--- js/shedule.js
+++ js/shedule.js
@@ -1,63 +1,57 @@
-const section2 = document.querySelector('.section2')
-const elementToHide = document.querySelector(".example")
-const ch1_fri = `
-                <div class="row justify-content-center text-center pt-3 flex-column">
-                <div class="col"> Физкультура (5101)<span class="time">9:00-10:30</span></div>
-                <div class="col"> ПЛИС лекция (1201)<span class="time">10:40-12:10</span></div>
-                <div class="col">ПЛИС сем  (4338у)<span class="time">12:50-14:20</span></span></div>
-                <div class="col"> ПЛИС лаба (4338у)<span class="time">14:30-16:00</span></div>
-                <div class="col"> ПЛИС лаба (4338у)<span class="time">16:10-17:40</span></div>
-                </div> `
+const schedules = {
+    ch1_fri: [
+      {subject: "Физкультура", room: "5101", time: "9:00-10:30"},
+      {subject: "ПЛИС лекция", room: "1201", time: "10:40-12:10"},
+      {subject: "ПЛИС сем", room: "4338у", time: "12:50-14:20"},
+      {subject: "ПЛИС лаба", room: "4338у", time: "14:30-16:00"},
+      {subject: "ПЛИС лаба", room: "4338у", time: "16:10-17:40"},
+    ],
+    zn1_fri: [
+      {subject: "Физкультура", room: "5101", time: "9:00-10:30"},
+      {subject: "фтд Углубленный C", room: "4330", time: "10:40-12:10"},
+      {subject: "фтд Углубленный C", room: "4330", time: "12:50-14:20"},
+    ],
+    // Другие расписания...
+  };
 
-// const ch1_third = `
-//                 <div class="row justify-content-center text-center pt-3 flex-column">
-//                 <div class="col"> Числаки лекция (1204)<span class="time">9:00-10:30</span></div>
-//                 <div class="col"> Электроника лекция (1203)<span class="time">10:40-12:10</span></div>
-//                 <div class="col">ООП лаба (4338у)<span class="time">12:50-14:20</span></span></div>
-//                 <div class="col"> ООП лаба (4338у)<span class="time">14:30-16:00</span></div>
-//                 <div class="col"> Электроника сем (3210)<span class="time">10:40-12:10</span></div>
-//                 </div> `
-
-const zn1_fri = `
-                <div class="row justify-content-center text-center pt-3 flex-column">
-                <div class="col"> Физкультура (5101)<span class="time">9:00-10:30</span></div>
-                <div class="col"> фтд Углубленный C (4330)<span class="time">10:40-12:10</span></div>
-                <div class="col"> фтд Углубленный C (4330)<span class="time">12:50-14:20</span></span></div>
-                </div> `
-
-
+  const section2 = document.querySelector('.section2')
 const firstch= document.querySelector('.firstch')
 const firstzn = document.querySelector('.firstzn')
 const fri = document.querySelector('.fri')
 let firstChClicked = false
-
-const shedule_day = (shedule1,shedule2)=>{
-    if(firstChClicked) 
-    section2.innerHTML = shedule1
-    else if (firstZnClicked)
-    section2.innerHTML = shedule2
-}
-firstch.addEventListener('click',()=>{
-    firstZnClicked = !firstZnClicked
-    firstChClicked = true})
-
 let firstZnClicked = false
-firstzn.addEventListener('click',()=>{
-    firstChClicked = !firstChClicked
-    firstZnClicked = true
-})
 
-fri.addEventListener('click', ()=>{shedule_day(ch1_fri,zn1_fri)})
+const renderSchedule = (schedule) => {
+  const rows = schedule.map(({ subject, room, time }) => {
+    return `
+      <div class="col">${subject} (${room})<span class="time">${time}</span></div>
+    `;
+  });
+  return `
+    <div class="row justify-content-center text-center pt-3 flex-column">
+      ${rows.join("")}
+    </div>
+  `;
+};
 
+const schedule_day = (schedule) => {
+  section2.innerHTML = renderSchedule(schedule);
+};
 
+firstch.addEventListener('click', () => {
+  firstChClicked = true;
+  firstZnClicked = false;
+});
 
+firstzn.addEventListener('click', () => {
+  firstChClicked = false;
+  firstZnClicked = true;
+});
 
-// const third = document.querySelector('.third')
-// third.addEventListener('click', ()=>{shedule_day(ch1_third)})
-
-
-
-
-
-
-
+fri.addEventListener('click', () => {
+  if (firstChClicked) {
+    schedule_day(schedules.ch1_fri);
+  } else if (firstZnClicked) {
+    schedule_day(schedules.zn1_fri);
+  }
+});
